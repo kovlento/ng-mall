f628956aae58c53e330ed483e9c723373589f472
kovlento <bluskytee@163.com>
2019-10-15 16:45:24 +0800

    tab组件

--- src/app/components/scrollable-tab/scrollable-tab.component.ts
+++ src/app/components/scrollable-tab/scrollable-tab.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, SimpleChanges, OnChanges, AfterContentInit } from '@angular/core';
 
 export interface TopMenu{
   title: string;
@@ -10,7 +10,7 @@ export interface TopMenu{
   templateUrl: './scrollable-tab.component.html',
   styleUrls: ['./scrollable-tab.component.css']
 })
-export class ScrollableTabComponent implements OnInit {
+export class ScrollableTabComponent implements OnInit,OnChanges, AfterContentInit {
 
   selectedIndex=-1;
   title = 'pinduoduo';
@@ -21,9 +21,25 @@ export class ScrollableTabComponent implements OnInit {
   constructor() {
     console.log('组件构造调用')
   }
+  
+  /* 
+    在组件"@Input"属性发生变化的时候调用
+    changes索引对象，key 是属性的名字
+  */
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("组件输入属性改变")
+    console.log(changes)
+  }
 
   ngOnInit() {
     console.log('组件初始化')
+  }
+
+  /* 
+    组件内容初始化
+  */
+  ngAfterContentInit(): void {
+    console.log("组件内容初始化")    
   }
 
   handleSelection(index: number){
